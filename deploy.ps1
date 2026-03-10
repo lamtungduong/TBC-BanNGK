95a78efdc81ff07278d008077350f3490667f3da
lamtungduong <contact@lamtungduong.com>
2026-03-10 17:58:50 +0700

    .

---
+++ deploy.ps1
@@ -0,0 +1,13 @@
+# Đổi thư mục sang project local
+Set-Location "E:\Projects\TBC-FnB"
+
+# 1. Commit & push code lên GitHub
+git add .
+git commit -m "."
+git push
+
+# 2. Đợi 5 giây
+Start-Sleep -Seconds 5
+
+# 3. SSH vào server và pull code + build + restart pm2
+ssh root1@192.168.10.79 "cd ~/TBC-FnB && git pull && npm install --production=false && npm run build && pm2 restart tbc-fnb"
